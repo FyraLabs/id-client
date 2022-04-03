b7b2f37e81c6b0b1814b9a371b18aba21fb83f4d
Lleyton Gray <lleyton@innatical.com>
2022-04-03 01:59:57 -0700

    tiiiired

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -4,6 +4,8 @@ import {
   faFingerprint,
   faKey,
   faMobileAlt,
+  faSave,
+  faWarning,
 } from "@fortawesome/free-solid-svg-icons";
 import {
   Button,
@@ -20,10 +22,19 @@ import {
   Tooltip,
   useModal,
   User,
+  Image,
+  Loading,
 } from "@nextui-org/react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/router";
+import { Auth } from "../../util/auth";
+import { FC, Suspense, useEffect, useMemo } from "react";
+import { useQuery } from "react-query";
+import { api } from "../../util/api";
+import dynamic from "next/dynamic";
+import dayjs from "dayjs";
 
 const HeadingRow = styled("div");
 
@@ -89,8 +100,46 @@ const UpdatePassword = () => {
   );
 };
 
+const Header = styled("div");
+const Circle = styled("div");
+
 const BasicInfo = () => {
+  const { token } = Auth.useContainer();
+  const user = useQuery(
+    ["me"],
+    async () =>
+      (
+        await api.get<{ id: string; email: string; name: string }>("/user/me", {
+          headers: {
+            Authorization: token!,
+          },
+        })
+      ).data,
+    {
+      enabled: !!token,
+    }
+  );
+
   const { setVisible, bindings } = useModal();
+
+  if (user.isLoading) return <Loading>Loading Basic Profile...</Loading>;
+  if (user.isError)
+    return (
+      <Error
+        css={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          textAlign: "center",
+          mw: 200,
+          gap: 5,
+          mx: "auto",
+        }}
+      >
+        <Icon icon={faWarning} fontSize={30} css={{ color: "$error" }} />
+        <Text>Failed to load basic user data, check console for more info</Text>
+      </Error>
+    );
 
   return (
     <>
@@ -103,14 +152,25 @@ const BasicInfo = () => {
       >
         <UpdatePassword />
       </Modal>
-      <Card css={{ mt: 20, mw: 500 }}>
+      <Card>
         <Text size={20} weight="bold">
           Basic Info
         </Text>
         <Spacer y={0.5} />
-        <Input label="Name" placeholder="Lea Gray" underlined />
+        <Input
+          label="Name"
+          placeholder="Lea Gray"
+          underlined
+          initialValue={user.data?.name}
+          contentRight={<Icon icon={faSave} />}
+        />
         <Spacer y={1} />
-        <Input label="Email" placeholder="lea@fyralabs.com" underlined />
+        <Input
+          label="Email"
+          placeholder="lea@fyralabs.com"
+          underlined
+          initialValue={user.data?.email}
+        />
         <Spacer y={1} />
         <Button color="warning" flat onClick={() => setVisible(true)}>
           Update Password
@@ -121,9 +181,192 @@ const BasicInfo = () => {
   );
 };
 
-const Index = () => {
+const SessionRow: FC<{}> = () => {
   return (
-    <Container>
+    <Collapse
+      contentLeft={
+        <FontAwesomeIcon icon={faMobileAlt} fixedWidth fontSize={20} />
+      }
+      title={<Text weight="bold">iPhone</Text>}
+      subtitle="Los Angeles, CA"
+    >
+      <Row>
+        <Col>
+          <Text weight="bold">IP</Text>
+          <Text>1.1.1.1</Text>
+        </Col>
+        <Col>
+          <Text weight="bold">Last Used</Text>
+          <Text>Yesterday, at 7:08am</Text>
+        </Col>
+      </Row>
+
+      <Spacer y={1} />
+
+      <Button color="error" css={{ w: "100%" }} auto>
+        Revoke Session
+      </Button>
+    </Collapse>
+  );
+};
+
+const SessionInfo = () => {
+  const { token } = Auth.useContainer();
+
+  const sessions = useQuery(
+    ["me", "session"],
+    async () =>
+      (
+        await api.get<
+          {
+            id: string;
+            ip: string;
+            userAgent: string;
+            createdAt: string;
+            lastUsedAt: string;
+          }[]
+        >("/user/me/session", {
+          headers: {
+            Authorization: token!,
+          },
+        })
+      ).data,
+    {
+      enabled: !!token,
+    }
+  );
+
+  const sessionData = useMemo(
+    () =>
+      sessions.data?.map((session) => ({
+        ...session,
+        // createdAt: dayjs.(session.createdAt),
+        // lastUsedAt: dayjs(session.lastUsedAt),
+      })),
+    [sessions]
+  );
+
+  console.log(sessionData);
+
+  if (sessions.isLoading) return <Loading>Loading Sessions...</Loading>;
+  if (sessions.isError)
+    return (
+      <Error
+        css={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          textAlign: "center",
+          mw: 200,
+          gap: 5,
+          mx: "auto",
+        }}
+      >
+        <Icon icon={faWarning} fontSize={30} css={{ color: "$error" }} />
+        <Text>Failed to load session data, check console for more info</Text>
+      </Error>
+    );
+
+  return (
+    <Card>
+      <Text size={20} weight="bold">
+        Sessions
+      </Text>
+      <Collapse.Group css={{ p: 0 }}>
+        <SessionRow />
+      </Collapse.Group>
+    </Card>
+  );
+};
+
+const Error = styled("div");
+const Icon = styled(FontAwesomeIcon);
+
+const Main = () => {
+  const router = useRouter();
+  const { token } = Auth.useContainer();
+  const user = useQuery(
+    ["me"],
+    async () =>
+      (
+        await api.get<{ id: string; email: string; name: string }>("/user/me", {
+          headers: {
+            Authorization: token!,
+          },
+        })
+      ).data,
+    {
+      enabled: !!token,
+    }
+  );
+
+  useEffect(() => {
+    if (!token) router.push("/login");
+  }, []);
+
+  if (!token) return <></>;
+
+  if (user.isLoading)
+    return (
+      <Container
+        css={{
+          h: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignContent: "center",
+        }}
+        fluid
+      >
+        <Loading>Loading User Data...</Loading>
+      </Container>
+    );
+
+  if (user.isError)
+    return (
+      <Container
+        css={{
+          h: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignContent: "center",
+        }}
+        fluid
+      >
+        <Error
+          css={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            textAlign: "center",
+            mw: 200,
+            gap: 5,
+          }}
+        >
+          <Icon icon={faWarning} fontSize={30} css={{ color: "$error" }} />
+          <Text>Failed to load user data, check console for more info</Text>
+        </Error>
+      </Container>
+    );
+
+  return (
+    <Container
+      css={{ mw: 600, display: "flex", flexDirection: "column", gap: 25 }}
+    >
+      <Header
+        css={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          mt: 25,
+        }}
+      >
+        <Image
+          src="https://cdn.discordapp.com/avatars/228736069091196928/cd771f5f824f380a4f82ce8f8e90be58.webp?size=512"
+          objectFit="cover"
+          css={{ borderRadius: "50%", aspectRatio: "1 / 1", maxW: 125 }}
+        />
+        <Text h1>Welcome Back, {user.data?.name}</Text>
+      </Header>
       {/* <Text size={30} weight="bold">
         General
       </Text>
@@ -142,64 +385,9 @@ const Index = () => {
       </Text> */}
 
       <BasicInfo />
+      <SessionInfo />
 
-      {/* <Card css={{ mt: 20, mw: 500 }}>
-        <Text size={20} weight="bold">
-          Sessions
-        </Text>
-        <Collapse.Group css={{ p: 0 }}>
-          <Collapse
-            contentLeft={
-              <FontAwesomeIcon icon={faMobileAlt} fixedWidth fontSize={20} />
-            }
-            title={<Text weight="bold">iPhone</Text>}
-            subtitle="Los Angeles, CA"
-          >
-            <Row>
-              <Col>
-                <Text weight="bold">IP</Text>
-                <Text>1.1.1.1</Text>
-              </Col>
-              <Col>
-                <Text weight="bold">Last Used</Text>
-                <Text>Yesterday, at 7:08am</Text>
-              </Col>
-            </Row>
-
-            <Spacer y={1} />
-
-            <Button color="error" css={{ w: "100%" }} auto>
-              Revoke Session
-            </Button>
-          </Collapse>
-
-          <Collapse
-            contentLeft={
-              <FontAwesomeIcon icon={faComputer} fixedWidth fontSize={20} />
-            }
-            title={<Text weight="bold">Mac</Text>}
-            subtitle="Saint Paul, MN"
-          >
-            <Row>
-              <Col>
-                <Text weight="bold">IP</Text>
-                <Text>0.0.0.0</Text>
-              </Col>
-              <Col>
-                <Text weight="bold">Last Used</Text>
-                <Text>Today, at 12:21pm</Text>
-              </Col>
-            </Row>
-
-            <Spacer y={1} />
-
-            <Button color="error" css={{ w: "100%" }} auto>
-              Revoke Session
-            </Button>
-          </Collapse>
-        </Collapse.Group>
-      </Card>
-      <Card css={{ mt: 20, mw: 500 }}>
+      <Card>
         <Text size={20} weight="bold">
           2FA
         </Text>
@@ -229,9 +417,10 @@ const Index = () => {
           </Collapse>
         </Collapse.Group>
         <Spacer y={0.5} />
-      </Card> */}
+      </Card>
     </Container>
   );
 };
 
-export default Index;
+//TODO: Find a better way of preventing SSR and client mismatch
+export default dynamic(async () => Main);
